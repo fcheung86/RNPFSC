e253f30e71f9d7397c8313231e267f88d1d42a3e
Justin To <justin.cn.to@gmail.com>
2012-12-04 01:17:06 -0500

    - added RPS buttons and a selection text view

--- src/com/example/rnpfsc/MainActivity.java
+++ src/com/example/rnpfsc/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.rnpfsc;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +20,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	public void onClick(View view) {
+		Button button = (Button) view;
+		String strSelection = button.getText().toString();
+
+		TextView selection = (TextView) findViewById(R.id.selection);
+		selection.setText("You selected " + strSelection);
+
+		// TODO get Facebook ID
+		// TODO send strSelection to NFC tag
 	}
 
 }
